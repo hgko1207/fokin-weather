8ad63397ee60e3dfcaefd928d3eddeb99b7fd0c5
hgko1207 <khkkhk1207@gmail.com>
2020-01-10 15:08:29 +0900

    Title and Subtitles

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
   getWeather = async (lat, lon) => {
     const { data: { main: { temp }, weather } }
       = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`);
-    this.setState({ isLoading: false, temp: temp, condition: weather[0].main, test: weather[0].main });
+    this.setState({ isLoading: false, temp: temp, condition: weather[0].main });
   };
 
   getLocation = async () => {
@@ -37,7 +37,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { isLoading, temp, condition, test } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} test={test} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
